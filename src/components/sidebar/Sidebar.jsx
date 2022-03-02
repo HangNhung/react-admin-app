b2db034f4069b58d43133533aacd542f17bd55df
Nhung Hang <hangbichnhung@gmail.com>
2022-03-02 18:07:36 +0700

    sidebar menu component

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,101 @@
+import {
+  FeedbackOutlined,
+  InsightsOutlined,
+  Inventory2Outlined,
+  LeaderboardOutlined,
+  LineStyle,
+  MailOutline,
+  ManageAccountsOutlined,
+  MessageOutlined,
+  PaidOutlined,
+  PersonOutline,
+  Report,
+  Timeline,
+  TrendingUp,
+} from "@mui/icons-material";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        {/* Dashboard */}
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <LineStyle className="sidebarIcon" />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUp className="sidebarIcon" />
+              Sales
+            </li>
+          </ul>
+        </div>
+        {/* Quick Menu */}
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <PersonOutline className="sidebarIcon" />
+              Users
+            </li>
+            <li className="sidebarListItem">
+              <Inventory2Outlined className="sidebarIcon" />
+              Products
+            </li>
+            <li className="sidebarListItem">
+              <PaidOutlined className="sidebarIcon" />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <LeaderboardOutlined className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+        {/* Notifications */}
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <MailOutline className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <FeedbackOutlined className="sidebarIcon" />
+              Feeback
+            </li>
+            <li className="sidebarListItem">
+              <MessageOutlined className="sidebarIcon" />
+              Messages
+            </li>
+          </ul>
+        </div>
+        {/* Staff */}
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <ManageAccountsOutlined className="sidebarIcon" />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <InsightsOutlined className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <Report className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
