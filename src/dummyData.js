fc043c015e12a307a12e3bc95b1418e996095f95
Nhung Hang <hangbichnhung@gmail.com>
2022-03-02 23:17:13 +0700

    make chart

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const data = [
+  {
+    name: "Jan",
+    "Active User": 4000,
+  },
+  {
+    name: "Feb",
+    "Active User": 1200,
+  },
+  {
+    name: "Mar",
+    "Active User": 2000,
+  },
+  {
+    name: "Apr",
+    "Active User": 1290,
+  },
+  {
+    name: "May",
+    "Active User": 400,
+  },
+  {
+    name: "Jun",
+    "Active User": 300,
+  },
+  {
+    name: "Jul",
+    "Active User": 900,
+  },
+  {
+    name: "Aug",
+    "Active User": 4000,
+  },
+  {
+    name: "Sep",
+    "Active User": 4000,
+  },
+  {
+    name: "Oct",
+    "Active User": 1213,
+  },
+  {
+    name: "Nov",
+    "Active User": 4000,
+  },
+  {
+    name: "Dec",
+    "Active User": 2090,
+  },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
+import Chart from "components/chart/Chart";
 import FeatureInfo from "components/featureInfo/FeatureInfo";
 import "./home.css";
+import { data } from "../../dummyData";
 
 export default function Home() {
   return (
     <div className="home">
       <FeatureInfo />
+      <Chart title="User Analytics" data={data} dataKey="Active User" grid />
     </div>
   );
 }
